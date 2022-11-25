5b92b1ca69de1f88a62d9349b5856ca33ce7ba4d
viniciustakeuchi <viniciustakeuchi1@gmail.com>
2022-11-25 18:16:00 -0300

    Apostila 04

---
+++ Impressora.java
@@ -0,0 +1,20 @@
+public class Impressora{
+    public void exibir (float ex1){
+        System.out.println(ex1);
+    }
+    public void exibir (float a, float b){
+        System.out.println(a+"," +b);
+    }
+    public void exibir (float c, String t1){
+        System.out.println(c+"," +t1);
+    }
+    public void exibir (String t2, float d){
+        System.out.println(t2+"," +d);
+    }
+    public void exibir (String t3, String t4, String t5){
+        System.out.println(t3+"," +t4+ "," +t5);
+    }
+    public void exibir (int x, int y, int z ){
+        System.out.println(x+"," +y+"," +z);
+    }
+}
---
+++ TesteImpressora.java
@@ -0,0 +1,13 @@
+public class TesteImpressora {
+  
+    public static void main (String[] args){
+        Impressora impre = new Impressora();
+        impre.exibir(1.2f);
+        impre.exibir(4f, 5f);
+        impre.exibir(6f, 2);
+        impre.exibir(4, 5, 9);
+        impre.exibir(10, 20, 30);
+        }
+    }
+    
+
